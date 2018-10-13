476fd71abb4d508e45d2d1b46a867eb2952466a6
Sergei Kulakov <s.culackow@gmail.com>
2018-10-14 00:07:14 +0300

    New project

--- lib/config.php
+++ lib/config.php
@@ -1,17 +1,13 @@
 <?php
-  // Вывод ошибок
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  ini_set('error_reporting', E_ALL);
 
   // База данных
   define('DB_DRIVER', 'mysql');
   define('DB_HOST', 'localhost');
   define('DB_CHARSET', 'utf8');
   // Изменить
-  define('DB_NAME', 'cf32186_faq'); // Название БД
-  define('DB_USER', 'cf32186_faq'); // Пользователь БД
-  define('DB_PASS', 'kng1262'); // Пароль пользованеля БД
+  define('DB_NAME', '****'); // Название БД
+  define('DB_USER', '****'); // Пользователь БД
+  define('DB_PASS', '****'); // Пароль пользованеля БД
 
 
   define('ROOT', dirname(__DIR__));
