74cc158bc04eded1b37177a4e9259ebe5afb7e56
Sergei Kulakov <s.culackow@gmail.com>
2018-10-15 12:04:06 +0300

    New project

---
+++ lib/config.php
@@ -0,0 +1,16 @@
+<?php
+
+  // База данных
+  define('DB_DRIVER', 'mysql');
+  define('DB_HOST', 'localhost');
+  define('DB_CHARSET', 'utf8');
+  // Изменить
+  define('DB_NAME', '***'); // Название БД
+  define('DB_USER', '***'); // Пользователь БД
+  define('DB_PASS', '***'); // Пароль пользованеля БД
+
+
+  //define('LOG', dirname(__DIR__). 'lib/log.txt');
+  define('LOG', 'lib/log.txt');
+  define('ROOT', dirname(__DIR__));
+  //define('ROOT', __DIR__);
